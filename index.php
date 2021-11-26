7da5c4505aa269767d119bf633328089ad6c5e01
web-tech-surgu <95065223+web-tech-surgu@users.noreply.github.com>
2021-11-26 12:04:01 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,10 @@
-<form>
-  <input name="login" type=text>
-  <input name="age" type=text>
-  </form>
-<?
-$login = "не определен";
-$age = "не определен";
-if(isset($_GET['login'])){
- 
-    $login = $_GET['login'];
-}
-if(isset($_GET['age'])){
- 
-    $age = $_GET['age'];
-}
-    echo "Ваш логин: $login <br> Ваш возраст: $age";
+<html>
+  <head>
+    <title>Тестируем PHP</title>
+  </head>
+  <body>
+<?php 
+	echo '<p>Привет, мир!</p>'; 
 ?>
+  </body>
+</html>
