146bdfc97f1762ce4a04343d633082b2bd37e656
web-tech-surgu <95065223+web-tech-surgu@users.noreply.github.com>
2021-11-26 11:53:25 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
-<?
-  echo "Hello world"
+<?php
+$login = "не определен";
+$age = "не определен";
+if(isset($_GET['login'])){
+ 
+    $login = $_GET['login'];
+}
+if(isset($_GET['age'])){
+ 
+    $age = $_GET['age'];
+}
+    echo "Ваш логин: $login <br> Ваш возраст: $age";
 ?>
