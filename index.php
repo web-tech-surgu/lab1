f8ad627fae4faefe9bcbdfbf6d3b390f0523319d
web-tech-surgu <95065223+web-tech-surgu@users.noreply.github.com>
2021-12-10 12:37:09 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,26 +1,4 @@
-<form method="post" action="qqq.php"> 	
-	<p>
-		<input type="submit" value="Send"></p> 
-<p>
-<select class="form-control" name="country" multiple>
- <option value="Австралия">Австралия</option>
- <option value="Австрия">Австрия</option>
- <option value="Азербайджан">Азербайджан</option>
- <option value="Албания">Албания</option>
- <option value="Алжир">Алжир</option>
-</select>
-	</p> 
-
-
-<select class="form-control" name="year">
-<?php 
-	for($i = 1900; $i <= date('Y'); $i++): 
-?>
-<option value="<?=$i?>"><?=$i?></option>
-<?php endfor; ?>
-</select>
-	
-	<form name="file_upload" method="POST" enctype="multipart/form-data"> 
+<form name="file_upload" method="POST" enctype="multipart/form-data"> 
 <label>Ваш аватар: 
 	<input type="file" name="avatar">
 </label> 
