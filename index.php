d7910333d9545eff18a693bf2c485a78316871c8
web-tech-surgu <95065223+web-tech-surgu@users.noreply.github.com>
2021-12-10 10:43:00 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,14 @@
 <body>
 <form action="select1.php" method="post"> 	
 	<p>
-		<select size="3" multiple name="hero[]"> 
-                        <option disabled>Выберите героя</option>
-			<option value="Чебурашка">Чебурашка</option>
-			<option selected value="Крокодил Гена">Крокодил Гена</option>
-			<option value="Шапокляк">Шапокляк</option>			
-		</select>
+<select class="form-control" name="country">
+ <option value="Австралия">Австралия</option>
+ <option value="Австрия">Австрия</option>
+ <option value="Азербайджан">Азербайджан</option>
+ <option value="Албания">Албания</option>
+ <option value="Алжир">Алжир</option>
+</select>
+
 	</p> 
 	<p>
 		<input type="submit" value="Отправить">
