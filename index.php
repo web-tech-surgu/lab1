f9cd2908f0dc458d23bf9fe6b61041f355575d66
web-tech-surgu <95065223+web-tech-surgu@users.noreply.github.com>
2021-12-10 10:47:06 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,6 @@
 <body>
 <form action="select1.php" method="post"> 	
-	<p>
-<select class="form-control" name="country" multiple>
- <option value="Австралия">Австралия</option>
- <option value="Австрия">Австрия</option>
- <option value="Азербайджан">Азербайджан</option>
- <option value="Албания">Албания</option>
- <option value="Алжир">Алжир</option>
-</select>
 
-	</p> 
 	<p>
 		<input type="submit" value="Отправить">
 	</p> 
