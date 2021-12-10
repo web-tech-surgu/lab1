77296a7a232278333e23e7cf50ccdef01e6e9f54
web-tech-surgu <95065223+web-tech-surgu@users.noreply.github.com>
2021-12-10 10:53:06 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,15 @@
 <form method="post"> 	
 	<p>
-		<input type="submit" value="Send">
-		<select name="hero"> 						 			
-			<option >Выберите героя</option>
-			<option value="Чебурашка">Чебурашка</option>
-			<option value="Крокодил Гена">Крокодил Гена</option>
-			<option value="Шапокляк">Шапокляк</option>			
-		</select>
+		<input type="submit" value="Send"></p> 
+<p>
+<select class="form-control" name="country">
+ <option value="Австралия">Австралия</option>
+ <option value="Австрия">Австрия</option>
+ <option value="Азербайджан">Азербайджан</option>
+ <option value="Албания">Албания</option>
+ <option value="Алжир">Алжир</option>
+</select>
+
 	</p> 
 </form>
 <?php
