50b312fa9f21aee7944459b54d1c7c2457202721
web-tech-surgu <95065223+web-tech-surgu@users.noreply.github.com>
2021-12-10 10:48:31 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,17 @@
-<body>
 <form action="select1.php" method="post"> 	
-
+	<p>
+		<select size="3" multiple name="hero[]"> 						 			
+			<option disabled>Выберите героя</option>
+			<option value="Чебурашка">Чебурашка</option>
+			<option selected value="Крокодил Гена">Крокодил Гена</option>
+			<option value="Шапокляк">Шапокляк</option>			
+		</select>
+	</p> 
 	<p>
 		<input type="submit" value="Отправить">
 	</p> 
 </form>
 <?php
-echo '<pre>';
+
 print_r($_POST);
 ?>
-</body>
