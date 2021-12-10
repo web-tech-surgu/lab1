6533ddbd3c6ffb996225a41f620856d60fc6f81e
web-tech-surgu <95065223+web-tech-surgu@users.noreply.github.com>
2021-12-10 12:36:20 +0500

    Update index.php

--- index.php
+++ index.php
@@ -19,3 +19,13 @@
 <option value="<?=$i?>"><?=$i?></option>
 <?php endfor; ?>
 </select>
+	
+	<form name="file_upload" method="POST" enctype="multipart/form-data"> 
+<label>Ваш аватар: 
+	<input type="file" name="avatar">
+</label> 
+<input type="submit" name="send" value="Отправить файл">
+</form>
+<?php
+print_r($_FILES[‘avatar']);
+?>
