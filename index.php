78c85325eb8ba27ea87769aec84e100be66d0677
web-tech-surgu <95065223+web-tech-surgu@users.noreply.github.com>
2022-04-20 20:32:17 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,5 @@
-<form name="file_upload" method="POST" enctype="multipart/form-data"> 
-<label>Ваш аватар: 
-	<input type="file" name="avatar">
-</label> 
-<input type="submit" name="send" value="Отправить файл">
-</form>
 <?php
-print_r($_FILES[‘avatar']);
-?>
+	$x = 10;
+	$x += --$x;
+	echo $x;
+ ?>
